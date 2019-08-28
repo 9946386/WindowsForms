a61968d4698be4ec32d7b227cea4ed2149b5f0d2
Anya Webb <9946386@student.toiohomai.ac.nz>
2019-08-28 14:59:04 +1200

    forms

---
+++ FormsExersize/FormsExersize/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace FormsExersize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblStar1 = new System.Windows.Forms.Label();
+            this.lblStar2 = new System.Windows.Forms.Label();
+            this.lblStar3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(201, 85);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(148, 20);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(201, 126);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(148, 20);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(201, 165);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(82, 20);
+            this.txtAge.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(374, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 277);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(201, 339);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(201, 310);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(127, 23);
+            this.btnDisplay.TabIndex = 5;
+            this.btnDisplay.Text = "Display all Records";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(201, 281);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(199, 69);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.TabIndex = 7;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(198, 110);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 8;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(198, 149);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 9;
+            this.lblAge.Text = "Age";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(198, 206);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 10;
+            // 
+            // lblStar1
+            // 
+            this.lblStar1.AutoSize = true;
+            this.lblStar1.Location = new System.Drawing.Point(248, 69);
+            this.lblStar1.Name = "lblStar1";
+            this.lblStar1.Size = new System.Drawing.Size(0, 13);
+            this.lblStar1.TabIndex = 11;
+            // 
+            // lblStar2
+            // 
+            this.lblStar2.AutoSize = true;
+            this.lblStar2.Location = new System.Drawing.Point(248, 110);
+            this.lblStar2.Name = "lblStar2";
+            this.lblStar2.Size = new System.Drawing.Size(0, 13);
+            this.lblStar2.TabIndex = 12;
+            // 
+            // lblStar3
+            // 
+            this.lblStar3.AutoSize = true;
+            this.lblStar3.Location = new System.Drawing.Point(220, 149);
+            this.lblStar3.Name = "lblStar3";
+            this.lblStar3.Size = new System.Drawing.Size(0, 13);
+            this.lblStar3.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStar3);
+            this.Controls.Add(this.lblStar2);
+            this.Controls.Add(this.lblStar1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblStar1;
+        private System.Windows.Forms.Label lblStar2;
+        private System.Windows.Forms.Label lblStar3;
+    }
+}
+
---
+++ FormsExersize/FormsExersize/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsExersize
+{
+    public partial class Form1 : Form
+    {
+        List<Deets> people = new List<Deets>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Deets newPerson = new Deets();
+            newPerson.Fname = txtFirstName.Text;
+            newPerson.Lname = txtLastName.Text;
+            newPerson.Age = int.Parse(txtAge.Text);
+            people.Add(newPerson);
+
+            txtAge.Text = null;
+            txtFirstName.Text = null;
+            txtLastName.Text = null;
+
+            lblOutput.Text = "Record created";
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "Displaying all records";
+
+            foreach(var x in people)
+            {
+                listBox1.Items.Add(x.DisplayName());
+                listBox1.Items.Add("Age: " + x.Age);
+                listBox1.Items.Add("*----------------------*");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtAge.Text = null;
+            txtLastName.Text = null;
+            txtFirstName.Text = null;
+            lblOutput.Text = null;
+            listBox1.Items.Clear();
+        }
+        
+    }
+}
